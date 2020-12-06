210f00ea5a1064fc70ac33415a5b7e4c22c98fb1
Vitalii Tsybulko <tsybulko.vitalii@gmail.com>
2020-12-06 15:27:55 +0200

    day6

---
+++ 2020/src/day6.py
@@ -0,0 +1,52 @@
+from typing import List, Set
+
+from io_utils import read_input_file
+
+
+def day6_1():
+    input_list = read_input_file("day6.txt", input_type=str)
+    answers = get_all_yes_answers_per_group(input_list)
+    amount_yes_answers=0
+    for answers_per_group in answers:
+        amount_yes_answers += len(answers_per_group)
+    return amount_yes_answers
+
+
+def get_all_yes_answers_per_group(input_list):
+    answers: List[Set[str]] = []
+    i = 0
+    for line in input_list:
+        if line == "\n":
+            i += 1
+        else:
+            if i == len(answers):
+                answers.append(set())
+            line = line.strip()
+            for char in line:
+                answers[i].add(char)
+    return answers
+
+
+def get_common_yes_answers_per_group(input_list):
+    answers: List[Set[str]] = []
+    i = 0
+    for line in input_list:
+        if line == "\n":
+            i += 1
+        else:
+            line = line.strip()
+            if i == len(answers):
+                answers.append(set(line))
+            else:
+                answers[i] = answers[i].intersection(line)
+    return answers
+
+
+
+def day6_2():
+    input_list = read_input_file("day6.txt", input_type=str)
+    answers = get_common_yes_answers_per_group(input_list)
+    amount_yes_answers=0
+    for answers_per_group in answers:
+        amount_yes_answers += len(answers_per_group)
+    return amount_yes_answers
--- 2020/src/main.py
+++ 2020/src/main.py
@@ -3,6 +3,7 @@ from day2 import day2_1, day2_2
 from day3 import day3_1, day3_2
 from day4 import day4_1, day4_2
 from day5 import day5_1, day5_2
+from day6 import day6_1, day6_2
 
 if __name__ == '__main__':
     # print(day1_1())
@@ -14,4 +15,6 @@ if __name__ == '__main__':
     # print(day4_1())
     # print(day4_2())
     # print(day5_1())
-    print(day5_2())
+    # print(day5_2())
+    # print(day6_1())
+    print(day6_2())
